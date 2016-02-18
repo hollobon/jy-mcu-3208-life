de2dd0cb40d699040c8459e891dbc52ada37a5eb
Pete Hollobon <pete@hollobon.com>
2016-02-18 21:45:08 +0000

    Key handling moved to libjymcu3208

--- life.c
+++ life.c
@@ -19,6 +19,7 @@
 #include "mq.h"
 #include "life.h"
 #include "timers.h"
+#include "io.h"
 
 #define M_KEY_DOWN 8
 #define M_KEY_UP 9
@@ -71,45 +72,7 @@ void set_up_timers(void)
 /* Interrupt handler for timer1. Polls keys and pushes events onto message queue. */
 ISR (TIMER1_CAPT_vect, ISR_NOBLOCK)
 {
-    typedef enum {down, up} key_state;
-    static key_state last_state[3] = {up, up, up};
-    static bool steady_state[3] = {true, true, true};
-    static bool key_repeating[3] = {false, false, false};
-    static uint16_t state_change_clock[3] = {0, 0, 0};
-    static uint16_t repeat_initial_delay[3] = {300, 300, 300};
-    static uint16_t repeat_subsequent_delay[3] = {200, 50, 200};
-
-    for (int key = 0; key < 3; key++) {
-        if (key_down(key)) {
-            if (last_state[key] == up) {
-                last_state[key] = down;
-                steady_state[key] = false;
-                state_change_clock[key] = clock_count;
-            }
-            else if (clock_count - state_change_clock[key] > (key_repeating[key] ? repeat_subsequent_delay[key] : repeat_initial_delay[key])) {
-                state_change_clock[key] = clock_count;
-                mq_put(msg_create(M_KEY_REPEAT, key));
-                key_repeating[key] = true;
-            }
-            else if (!steady_state[key] && clock_count - state_change_clock[key] > DEBOUNCE_TIME) {
-                mq_put(msg_create(M_KEY_DOWN, key));
-                steady_state[key] = true;
-            }
-        }
-        else {
-            if (last_state[key] == down) {
-                last_state[key] = up;
-                steady_state[key] = false;
-                state_change_clock[key] = clock_count;
-            }
-            else if (!steady_state[key] && clock_count - state_change_clock[key] > DEBOUNCE_TIME) {
-                mq_put(msg_create(M_KEY_UP, key));
-                steady_state[key] = true;
-                key_repeating[key] = false;
-            }
-        }
-    }
-
+    handle_keys();
     handle_timers();
 }
 
